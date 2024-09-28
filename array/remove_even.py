64b3b13dbedfa03f049556d398159ca51af693f5
avinashcemk2010 <avinashcemk2010@gmail.com>
2024-09-28 12:16:20 +0530

    initial commit for remove even from array code

---
+++ array/remove_even.py
@@ -0,0 +1,11 @@
+def remove_even(ls: list):
+    i = 0
+    while(i < len(ls)):
+        if ls[i] % 2 == 0:
+            ls.pop(i) 
+        else:
+            i += 1
+        
+    print(ls) 
+
+remove_even([1,2,6,4,4,4,4,4,4,4,5,6,7,45,43])
