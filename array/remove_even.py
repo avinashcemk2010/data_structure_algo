69686b553cb23b320147022dcc6bc8af9aa1bade
avinashcemk2010 <avinashcemk2010@gmail.com>
2024-09-28 12:27:34 +0530

    added a comment

--- array/remove_even.py
+++ array/remove_even.py
@@ -2,6 +2,7 @@ def remove_even(ls: list):
     i = 0
     while(i < len(ls)):
         if ls[i] % 2 == 0:
+            #pop value at index i
             ls.pop(i) 
         else:
             i += 1
