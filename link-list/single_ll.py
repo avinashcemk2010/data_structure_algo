70cf04feb5290d13dbf7257f673b5e04de73a657
avinashcemk2010 <avinashcemk2010@gmail.com>
2024-09-29 11:07:38 +0530

    modified code to fix a bug while inserting element at a pos

--- link-list/single_ll.py
+++ link-list/single_ll.py
@@ -33,11 +33,15 @@ class LinkedList:
     def len(self):
         current_node = self.head
         count = 0
-        while current_node:
-            count += 1
-            current_node = current_node.next
 
-        return count
+        if self.head:
+            while current_node:
+                count += 1
+                current_node = current_node.next
+
+            return count
+        else:
+            return 0
 
     def insert_at_pos(self, data, pos):
         len_of_ll = self.len()
@@ -46,34 +50,26 @@ class LinkedList:
 
         if pos == 1:
             new_node = Node(data)
-            if self.head == None:
-                self.head = new_node
-            else:
-                new_node.next = self.head.next
-                self.head = new_node
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current_node = self.head
+            count = 1
+            while count < pos-1:
+                current_node = current_node.next
+                count += 1
 
-            return
-
-        current_node = self.head
-        count = 1
-        while current_node:
-            count += 1
-
-            if pos == count:
-                new_node = Node(data)
-                new_node.next = current_node.next
-                current_node.next = new_node 
-
-            current_node = current_node.next
+            new_node = Node(data)
+            new_node.next = current_node.next
+            current_node.next = new_node 
         
-        return
 
     def delete(self, pos):
         current_node = self.head
         count = 1
 
         if pos == 1:
-            head = head.next
+            self.head = self.head.next
         else:
             while current_node:
                 if count == pos-1:
@@ -115,12 +111,16 @@ print(f'length of link list:{ll.len()}')
 
 # insert 29 at pos 4
 ll.insert_at_pos(29, 4)
+print(f'after insertin 29 at 4th pos')
 ll.print_ll()
 ll.insert_at_pos(299, 12)
+print(f'after insertin 299 at 12th pos')
 ll.print_ll()
 ll.insert_at_pos(299, 115)
+print(f'after insertin 299 at 115th pos')
 ll.print_ll()
 ll.insert_at_pos(0, 1)
+print(f'after insertin 0 at 1st pos')
 ll.print_ll()
 
 # delete an element at i th position
