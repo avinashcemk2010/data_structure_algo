843ac201ef527089b2fe00168cbb31f94a66c3be
avinashcemk2010 <avinashcemk2010@gmail.com>
2024-10-03 21:24:49 +0530

    added code to add and delete elements from the list

--- link-list/doubly_ll.py
+++ link-list/doubly_ll.py
@@ -61,6 +61,38 @@ class DoublyLinkList:
         self.head = new_node
 
 
+    def delete_first_element(self):
+        if self.get_length() == 0:
+            return
+
+        temp = self.head
+        if self.head == self.tail:
+            self.tail = None
+        else:
+            self.head.next.previous = None
+
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        return temp 
+
+    
+    def delete_last_element(self):
+        temp = self.tail
+        if self.head == self.tail:
+            self.head = None
+        else:
+            self.tail.previous.next = None
+
+        self.tail = self.tail.previous 
+        temp.previous = None
+
+        return temp 
+
+
+
+
+
 dll = DoublyLinkList()
 for i in range(10):
     dll.append(i)
@@ -70,6 +102,10 @@ dll.print_backward_ll()
 dll.get_length()
 
 dll.add_at_first(45)
+dll.print_forward_ll()
+
+dll.delete_first_element()
+dll.delete_first_element()
 dll.print_forward_ll()
 
     
