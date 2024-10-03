d5f55dc19d5577f998fea42dc659391a2967e7a0
avinashcemk2010 <avinashcemk2010@gmail.com>
2024-10-03 21:05:47 +0530

    initial commit for doubly link list

---
+++ link-list/doubly_ll.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.next = None
+        self.previous = None
+        self.data = data
+
+class DoublyLinkList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, data):
+        new_node = Node(data)
+        new_node.next = None
+        new_node.previous = None 
+        if self.tail:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+        else:
+            self.head = new_node
+            
+        self.tail = new_node
+        self.length += 1
+
+    def print_forward_ll(self):
+        if self.head == None or self.tail == None:
+            return
+
+        current = self.head
+        while current:
+            print(current.data, end="-->")
+            current = current.next
+        
+        print()
+
+    def print_backward_ll(self):
+        if self.head == None or self.tail == None:
+            return
+
+        last = self.tail
+        while last:
+            print(last.data, end="-->")
+            last = last.previous
+        
+        print()
+
+    
+    def get_length(self):
+        print(self.length)
+
+    
+    def add_at_first(self, data):
+        new_node = Node(data)
+        if self.get_length() == 0:
+            self.tail = new_node
+        else:
+            self.head.previous = new_node
+
+        new_node.next = self.head
+        self.head = new_node
+
+
+dll = DoublyLinkList()
+for i in range(10):
+    dll.append(i)
+
+dll.print_forward_ll()
+dll.print_backward_ll()
+dll.get_length()
+
+dll.add_at_first(45)
+dll.print_forward_ll()
+
+    
+
+
+            
+
+
+    
