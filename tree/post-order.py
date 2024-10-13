2c9443c1551946a5650b4d24b85dfefaca4ebfae
avinashcemk2010 <avinashcemk2010@gmail.com>
2024-10-13 12:33:31 +0530

    post order traversal using iterative approach

--- tree/post-order.py
+++ tree/post-order.py
@@ -10,7 +10,38 @@ def post_order_recursion(root):
     print(root.data, end="-->")
 
 
+def post_order_iterative(root):
+    if root is None:
+        return
+
+    # Create two stacks
+    stack1 = []
+    stack2 = []
+
+    # Push the root to the first stack
+    stack1.append(root)
+
+    # Run while the first stack is not empty
+    while stack1:
+        # Pop an item from stack1 and push it to stack2
+        node = stack1.pop()
+        stack2.append(node)
+
+        # Push the left and right children of the popped node to stack1
+        if node.left:
+            stack1.append(node.left)
+        if node.right:
+            stack1.append(node.right)
+
+    # The elements in stack2 are in postorder, so print them
+    while stack2:
+        node = stack2.pop()
+        print(node.data, end='-->')
+
+
 createTree()
 post_order_recursion(TreeNode.root)
+print()
+post_order_iterative(TreeNode.root)
 
 
